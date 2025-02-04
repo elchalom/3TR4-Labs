ef501c36afeeacd259e3daca7795ac5fc8378d9c
munj4 <munj4@mcmaster.ca>
2025-02-04 14:20:57 -0500

    finalized the matlab code

--- Lab1/Updated Filter Code/triangular_filtering.m
+++ Lab1/Updated Filter Code/triangular_filtering.m
@@ -3,9 +3,9 @@ clc
 clear all
 hold off
 
-f0=500;     %fundamental freq of input sqaure wave
+f0=800;     %fundamental freq of input sqaure wave
 T0 = 1/f0;  %period 
-tstep = 0.005*T0;
+tstep = 0.01*T0;
 no_sample = 3*T0/tstep + 1; %no. of samples  within  3*T0
 
 %tt = -0.5*T0:tstep:0.5*T0;
@@ -30,7 +30,7 @@ c_in = zeros(size(nvec)); %fourier coefficients
 A = 2;
 for n = nvec
     m = n+N+1;
-    c_in(m) = (2*A / (n * pi)) * sin((n * pi) / 2);
+    c_in(m) = (A/(1i*n*pi))*(1-cos(n*pi));
     
     if (n == 0)
       c_in(m) = 0.0;
@@ -62,7 +62,7 @@ pause
 %% Designing the 2nd order Butterworth filter
 
 
-R=10e3; %10k ohms
+R=10000; %10k ohms
 C=1e-8; %10 nF
 fc=1/(2*pi*R*C);     %cutoff freq of filter
 %fc = 5000;
@@ -83,22 +83,15 @@ title('magnitude spectrum of filter output and input')
 Ha = gca;
 set(Ha,'Fontsize',16)
 legend('input','output')
-% pause
-
-% figure(5)
-% Hp1=plot(f,angle(c_out));
-% set(Hp1,'LineWidth',2)
-% Ha = gca;
-% set(Ha,'Fontsize',16)
-% title('phase spectrum of output')
-% axis([-0.1e4 0.1e4 -pi pi])
+pause
+%% 
 
 figure(5)
 stem(f,angle(c_in),'r','LineWidth',2);
 hold on
 stem(f,angle(c_out),'b','LineWidth',2);
 hold off
-axis([-0.1e4 0.1e4 -pi pi])
+axis([-0.8e4 0.8e4 -pi pi])
 Ha = gca;
 set(Ha,'Fontsize',16)
 title('phase spectrum of input and output')
